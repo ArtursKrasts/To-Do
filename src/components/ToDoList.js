b9124cdec5fde12cc4a6b18d222856e7881950d5
ArtursKrasts <AKSturmovik@gmail.com>
2020-09-03 12:16:10 +0300

    qqq

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../App.css';
-import ToDoObj from './ToDoObj.js'
-import { Button, Input } from '@material-ui/core';
+import { Button, Input, Checkbox } from '@material-ui/core';
+import { v4 as uuidv4 } from 'uuid';
 
 class ToDoList extends React.Component{
     constructor(props){
         super(props)
 
         this.state = {
-            listOfStuff: [],
+            listCheckedKeys: [],
             listToDo: [],
             newText: ""
         }
@@ -16,13 +16,18 @@ class ToDoList extends React.Component{
     }
 
     addText(){
+        const toDoKey=uuidv4();
+
         this.setState({
-            listOfStuff: this.state.listOfStuff.concat(this.state.newText)
-        },
-        () => {
-            this.setState({
-                listToDo: this.state.listOfStuff.map(ToDo => <ToDoObj text = {ToDo}/>)
-            })
+            listToDo: this.state.listToDo.concat(
+              <div key={toDoKey}>
+                <Checkbox
+                  color="primary"
+                  inputProps={{ 'aria-label': 'Checkbox' }}
+                  onChange={event => this.checked(toDoKey)}
+                />
+                {this.state.newText}
+              </div>)
         })
     }
 
@@ -30,6 +35,47 @@ class ToDoList extends React.Component{
         this.setState({
             newText: newText
         })
+    }
+
+    delete(){
+        console.log("ToDoList_delete");
+        console.log(this.state.listToDo.concat("before"));
+        this.state.listCheckedKeys.map(Key => {
+            this.setState(prevState => ({
+                listToDo: prevState.listToDo.filter((item) => item.key !== Key)
+            }),
+                () => {
+                    console.log(this.state.listToDo.concat("after_removed"));
+                }
+            )
+        })
+    }
+
+    checked(toDoKey){
+        console.log("ToDoList_checked:"+{toDoKey});
+        var test = true;
+        console.log(this.state.listCheckedKeys.concat("before"));
+        this.state.listCheckedKeys.map(Key => {
+            if(Key === toDoKey){
+                this.setState(prevState => ({
+                        listCheckedKeys: prevState.listCheckedKeys.filter((item) => item !== toDoKey)
+                }),
+                    () => {
+                        console.log(this.state.listCheckedKeys.concat("after_removed"));
+                    }
+                )
+                test = false;
+            }
+        })
+        if(test){
+            this.setState(prevState => ({
+                listCheckedKeys: prevState.listCheckedKeys.concat(toDoKey)
+            }),
+                () => {
+                    console.log(this.state.listCheckedKeys.concat("after_added"));
+                }
+            )
+        }  
     }
 
     render(){
@@ -45,7 +91,8 @@ class ToDoList extends React.Component{
                         type="string" 
                         onChange={event => this.changeText(event.target.value)}/>
                 </form>
-                <Button onClick={() => this.addText()} variant="contained" color="primary">Add</Button>
+                <p><Button onClick={() => this.addText()} variant="contained" color="primary">Add</Button></p>
+                <p><Button onClick={() => this.delete()} variant="contained" color="primary">Delete selected</Button></p>
             </div>
         )
   }
